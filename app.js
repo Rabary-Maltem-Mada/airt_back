f7fcf4d7b00577686207a622440ccb0711595fcb
unknown <rbrstart@gmail.com>
2019-05-09 18:10:26 +0300

    API list

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var http = require('http'),
     mongoose = require('mongoose');
 
 var isProduction = process.env.NODE_ENV === 'production';
-
+const listEndpoints = require('express-list-endpoints')
 // Create global app object
 var app = express();
 
@@ -89,4 +89,5 @@ app.use(function(err, req, res, next) {
 // finally, let's start our server...
 var server = app.listen( process.env.PORT || 3000, function(){
   console.log('Listening on port ' + server.address().port);
+  console.log(listEndpoints(app));
 });
